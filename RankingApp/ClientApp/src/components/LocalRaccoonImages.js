b3077beec8ea6083a4b61156cd655022505ff020
MandeepKMann <mandeep_mann30@hotmail.com>
2023-02-24 15:37:31 -0500

    added local raccoon images :raccoon:

---
+++ RankingApp/ClientApp/src/components/LocalRaccoonImages.js
@@ -0,0 +1,25 @@
+﻿import DeadRaccoonTO from '../images/localRaccoons/Cheeto.jpg';
+import HowardLotor from '../images/localRaccoons/Evil.jpg';
+import JacksonTheRaccoon from '../images/localRaccoons/Loaf.pjeg';
+import Meeko from '../images/localRaccoons/Pizza.jpg';
+import MrPorter from '../images/localRaccoons/RockingHorse.jpg';
+import Raccacoonie from '../images/localRaccoons/Sleepy.jpg';
+import RJ from '../images/localRaccoons/Smiling.jpg';
+import RocketRaccoon from '../images/localRaccoons/Standing.jpg';
+import SlyCooper from '../images/localRaccoons/Strong.jpg';
+import TomNook from '../images/localRaccoons/Teddy.jpg';
+
+const LocalRaccoonImageArray = [
+    { id: 1, image: Cheeto },
+    { id: 2, image: Evil },
+    { id: 3, image: Loaf },
+    { id: 4, image: Pizza },
+    { id: 5, image: RockingHorse },
+    { id: 6, image: Sleepy },
+    { id: 7, image: Smiling },
+    { id: 8, image: Standing },
+    { id: 9, image: Strong },
+    { id: 10, image: Teddy }
+]
+
+export default LocalRaccoonImageArray;
