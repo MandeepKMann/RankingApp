5efda4c5af10c409b8b4f008e96760ddd7beb186
MandeepKMann <mandeep_mann30@hotmail.com>
2023-02-26 22:34:48 -0500

    changed up some css for links

---
+++ RankingApp/ClientApp/src/components/Footer.js
@@ -0,0 +1,9 @@
+﻿const Footer = () => {
+    return (
+        <footer>
+            <p>Created by <a href="" target="_blank">Mandeep Mann</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
