58ed0e98cb47e5d72f90d0029387ed32771583e1
MandeepKMann <mandeep_mann30@hotmail.com>
2023-02-27 00:01:21 -0500

    condensed some text on home page

--- RankingApp/ClientApp/src/components/Home.js
+++ RankingApp/ClientApp/src/components/Home.js
@@ -12,11 +12,8 @@ export class Home extends Component {
                 <Container className="homeFlex">
                     <div className="homeTextContainer">
                         <h1>Welcome to Raccoon Ranker!</h1>
-                        <p>Are you looking for a fun and easy way to rank raccoons? Look no further!</p>
-                        <p>Raccoon Ranker is the perfect place where everyone can come together and rate different kinds of raccoons.</p>
-                        <p>We offer an extensive list of raccoons from all over the world, so there's something for everybody.</p>
-                        <p>Check out our Famous Raccoons to rank some of our more popular furry friends.</p>
-                        <p>You can also check out Local Raccoons to see some up and coming stars!</p>
+                        <p>Are you looking for a fun and easy way to rank raccoons? Look no further! Raccoon Ranker is the perfect place where everyone can come together and rate different kinds of raccoons. We offer an extensive list of raccoons from all over the world, so there's something for everybody.</p>
+                        <p>Check out our Famous Raccoons to rank some of our more popular furry friends. You can also check out Local Raccoons to see some up and coming stars!</p>
                         <div className="links">
                             <Link to="/rank-famous">Famous Raccoons</Link>
                             <Link to="/rank-local">Local Raccoons</Link>
